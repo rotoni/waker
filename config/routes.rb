b2047e177ee88afb44536754f94f2d0c55846d00
uu4k <t.shimizu1115@gmail.com>
2017-11-16 08:43:10 +0900

    add route for create user.

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
 
   resources :maintenances
 
-  resources :users, only: [:index, :show, :destroy] do
+  resources :users, only: [:index, :show, :destroy, :new, :create] do
     member do
       patch 'activation'
       patch 'deactivation'
